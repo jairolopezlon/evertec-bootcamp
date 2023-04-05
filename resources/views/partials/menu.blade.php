617f2ad18e9bfe4c9bac63bbc241616cfe714feb
Jairo Lopez L <jairolop88@gmail.com>
2023-04-05 01:18:03 -0500

    feat: menu is update to conditional menu, only show options according autentication status

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,4 +1,25 @@
-<a href="/">home</a>
-<a href="/dashboard">dashboard</a>
-<a href="/login">login</a>
-<a href="/signup">signup</a>
+<nav class="nav-main">
+    <ul>
+        <li>
+            <a href="/">home</a>
+        </li>
+        @auth
+            <li>
+                <a href="/dashboard">dashboard</a>
+            </li>
+        @endauth
+        @guest
+            <li>
+                <a href="/login">login</a>
+            </li>
+            <li>
+                <a href="/signup">signup</a>
+            </li>
+        @endguest
+        @auth
+            <li>
+                <a href="/logout">Logout</a>
+            </li>
+        @endauth
+    </ul>
+</nav>
