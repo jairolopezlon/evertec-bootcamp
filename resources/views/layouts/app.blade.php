2ae50a42006dc013eb3895724512121885573667
Jairo Lopez L <jairolop88@gmail.com>
2023-05-05 02:59:04 -0500

    chore: update page title shown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Undefined title') | Ecommerce</title>
+    <title>@yield('title', 'Undefined title') | Everstore</title>
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
     @stack('head')
 </head>
