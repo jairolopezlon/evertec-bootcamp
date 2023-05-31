c8ce12aab823a4b70791908fcd50c009122ef31b
Jairo Lopez L <jairolop88@gmail.com>
2023-05-31 11:20:02 -0500

    feat: productRepository registred

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Products\Domain\Repositories\ProductRepository;
+use Src\Products\Infrastructure\Repository\Eloquent\EloquentProductRespositoryImpl;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ProductRepository::class, EloquentProductRespositoryImpl::class);
     }
 
     /**
