2a53fa1ed99ebf3ac6df2ea6940ac74b2c25a456
Jairo Lopez L <jairolop88@gmail.com>
2023-06-02 23:48:44 -0500

    chore: encode data to be able to accessing to it from dtos

--- resources/views/pages/ecommerce/products/productDetail.blade.php
+++ resources/views/pages/ecommerce/products/productDetail.blade.php
@@ -4,5 +4,5 @@
 @section('title', 'Ecommerce Products List')
 @section('ecommerce-content')
     <h1>Product Detail Ecommercedddddddddd</h1>
-    <product-item :product="{{ $product }}"></product-item>
+    <product-item :product="{{ json_encode($product) }}"></product-item>
 @endsection
