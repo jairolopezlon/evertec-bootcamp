cd1572354360b1e730ef51378e7c2c9925677be7
Jairo Lopez L <jairolop88@gmail.com>
2023-06-11 14:22:02 -0500

    feat: checkout view added

---
+++ resources/ts/components/ecommerce/shopping/checkout/ListItemsCheckout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col gap-4 grow-[1]">
+    <item-checkout-card v-for="cartItem in getItems" :key="cartItem.productId" :productId="cartItem.productId" :token="props.token"></item-checkout-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useShoppingCart } from '@/composables';
+
+const { getItems } = useShoppingCart()
+
+const props = defineProps({
+  token: {
+    type: String,
+    default: ''
+  }
+})
+
+</script>
