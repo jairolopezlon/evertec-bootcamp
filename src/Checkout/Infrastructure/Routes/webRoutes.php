a83b09eef9f6e648a09859e2c8d5baa704eed715
Jairo Lopez L <jairolop88@gmail.com>
2023-06-12 06:09:34 -0500

    fix: avoid that guest user go to checkout

--- src/Checkout/Infrastructure/Routes/webRoutes.php
+++ src/Checkout/Infrastructure/Routes/webRoutes.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 use Src\Checkout\Infrastructure\Controllers\CheckoutController;
 
 Route::get('/checkout', [CheckoutController::class, 'index'])
+    ->middleware(['auth'])
     ->name('ecommerce.checkout.index');
