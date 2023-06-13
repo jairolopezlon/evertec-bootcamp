8e04553d6ba1f14417ee408e838bb36c3ad1c092
Jairo Lopez L <jairolop88@gmail.com>
2023-06-13 15:52:11 -0500

    feat: order creation logic added

--- src/Orders/Domain/Repositories/OrderRepositoryInterface.php
+++ src/Orders/Domain/Repositories/OrderRepositoryInterface.php
@@ -3,8 +3,20 @@
 namespace Src\Orders\Domain\Repositories;
 
 use Src\Orders\Domain\Models\Order;
+use Src\Shared\Domain\Types\Types;
+use Src\Orders\Domain\Enums\PaymentProviderEnum;
+use Src\Orders\Domain\Enums\PaymentCurrencyEnum;
 
+/**
+ * @phpstan-import-type ValidatedItemShoppingCartNative from Types
+ */
 interface OrderRepositoryInterface
 {
-    public function createOrder(Order $order): Order;
+    /**
+     * @param PaymentProviderEnum $paymentProvider
+     * @param PaymentCurrencyEnum $paymentCurrency
+     * @param ValidatedItemShoppingCartNative $shoppingCartData
+     * @return Order
+     */
+    public function createOrder($paymentProvider, $paymentCurrency, $shoppingCartData): Order;
 }
--- src/Orders/Infrastructure/Controllers/OrderController.php
+++ src/Orders/Infrastructure/Controllers/OrderController.php
@@ -3,17 +3,41 @@
 namespace Src\Orders\Infrastructure\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Src\Orders\Application\Actions\OrderCreateAction;
+use Src\Orders\Domain\Enums\PaymentCurrencyEnum;
+use Src\Orders\Domain\Enums\PaymentProviderEnum;
+use Src\Orders\Infrastructure\Persistence\Eloquent\EloquentOrderEntity;
 
 class OrderController
 {
-    public function index(Request $request): View
-    {
-        return view('pages.ecommerce.orders.orderList');
+    public function __construct(
+        private readonly OrderCreateAction $orderCreateAction
+    ) {
     }
 
-    public function create(Request $request): View
+    public function index(Request $request): View
     {
-        return view('pages.ecommerce.orders.orderList');
+        $orders = EloquentOrderEntity::all();
+
+        return view('pages.ecommerce.orders.orderList', compact('orders'));
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'paymentProvider' => 'required|in:' . implode(',', PaymentProviderEnum::enumToArray()),
+            'currency' => 'required|in:' . implode(',', PaymentCurrencyEnum::enumToArray())
+        ]);
+
+        $shoppingCartData = $request->session()->get('shoppingCart');
+
+        $paymentProvider = PaymentProviderEnum::enumFromValue($validatedData['paymentProvider']);
+        $paymentCurrency = PaymentCurrencyEnum::enumFromValue($validatedData['currency']);
+
+        $orderData = ($this->orderCreateAction)($paymentProvider, $paymentCurrency, $shoppingCartData);
+
+        return redirect()->route('ecommerce.orders.index');
     }
 }
