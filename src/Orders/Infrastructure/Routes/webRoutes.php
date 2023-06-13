9a900ffb3c82839e96ceb7fa8605e9bbc53b66d0
Jairo Lopez L <jairolop88@gmail.com>
2023-06-13 15:52:31 -0500

    chore: routes added

--- src/Orders/Infrastructure/Routes/webRoutes.php
+++ src/Orders/Infrastructure/Routes/webRoutes.php
@@ -5,5 +5,5 @@ namespace Src\Orders\Infrastructure\Routes;
 use Illuminate\Support\Facades\Route;
 use Src\Orders\Infrastructure\Controllers\OrderController;
 
-Route::get('/order', [OrderController::class, 'index']);
-Route::get('/order', [OrderController::class, 'create']);
+Route::get('/orders', [OrderController::class, 'index'])->middleware(['auth'])->name('ecommerce.orders.index');
+Route::post('/orders', [OrderController::class, 'store'])->middleware(['auth'])->name('ecommerce.orders.store');
