9392cfb284bf25219f7b3b546414b4b4f6510091
Jairo Lopez L <jairolop88@gmail.com>
2023-06-14 20:00:14 -0500

    fix: just return order of login user

--- src/Orders/Infrastructure/Controllers/OrderController.php
+++ src/Orders/Infrastructure/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace Src\Orders\Infrastructure\Controllers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Src\Orders\Application\Actions\OrderCreateAction;
 use Src\Orders\Domain\Enums\PaymentCurrencyEnum;
 use Src\Orders\Domain\Enums\PaymentProviderEnum;
@@ -19,7 +20,8 @@ class OrderController
 
     public function index(Request $request): View
     {
-        $orders = EloquentOrderEntity::all();
+        $userId = Auth::user()->id;
+        $orders = EloquentOrderEntity::where('user_id', $userId)->get();
 
         return view('pages.ecommerce.orders.orderList', compact('orders'));
     }
@@ -27,8 +29,8 @@ class OrderController
     public function store(Request $request): RedirectResponse
     {
         $validatedData = $request->validate([
-            'paymentProvider' => 'required|in:' . implode(',', PaymentProviderEnum::enumToArray()),
-            'currency' => 'required|in:' . implode(',', PaymentCurrencyEnum::enumToArray())
+            'paymentProvider' => 'required|in:'.implode(',', PaymentProviderEnum::enumToArray()),
+            'currency' => 'required|in:'.implode(',', PaymentCurrencyEnum::enumToArray()),
         ]);
 
         $shoppingCartData = $request->session()->get('shoppingCart');
