f006a9b238f70f6bc294a0f21f4cc8c2eebb4ee4
Jairo Lopez L <jairolop88@gmail.com>
2023-06-16 07:55:24 -0500

    feat: function moved to reactive functions

--- resources/ts/components/ecommerce/shopping/shoopingCart/ModifierCartAmountButton.vue
+++ resources/ts/components/ecommerce/shopping/shoopingCart/ModifierCartAmountButton.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="modifier-cart-amount-button">
-    <button @click="() => decrementCartItem(props.productId, 1, props.token)">
+    <button class="bg-indigo-50" @click="() => decrementCartItem(props.productId, 1, props.token)">
       <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path clip-rule="evenodd" fill-rule="evenodd" d="M5.25 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5H6a.75.75 0 01-.75-.75z"></path>
       </svg>
     </button>
     <span>{{ cartItem.amount }}</span>
-    <button @click="() => incrementCartItem(props.productId, 1, props.token)">
+    <button class="bg-indigo-50" @click="() => incrementCartItem(props.productId, 1, props.token)">
       <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path clip-rule="evenodd" fill-rule="evenodd" d="M12 5.25a.75.75 0 01.75.75v5.25H18a.75.75 0 010 1.5h-5.25V18a.75.75 0 01-1.5 0v-5.25H6a.75.75 0 010-1.5h5.25V6a.75.75 0 01.75-.75z"></path>
       </svg>
