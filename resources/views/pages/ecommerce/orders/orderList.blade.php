122397a04bcc5f6e97b3517f334a5873cc993bbb
Jairo Lopez L <jairolop88@gmail.com>
2023-06-16 07:50:38 -0500

    feat: order page added

--- resources/views/pages/ecommerce/orders/orderList.blade.php
+++ resources/views/pages/ecommerce/orders/orderList.blade.php
@@ -2,6 +2,45 @@
 
 @section('title', 'Orders List')
 @section('ecommerce-content')
-    <h1>Order List</h1>
+    <div class="flex justify-center">
+        <div class="flex flex-col py-4" style="width: min(900px, 100%)">
+            <h1 class="font-bold text-xl">Orders</h1>
+            <div class="flex flex-col gap-2">
+                @foreach ($orders as $order)
+                    <div class="border-[1px] border-indigo-100 p-4 rounded-lg">
+                        <div>
+                            <div>
 
+                            </div>
+                            <div>
+                                <span>Order Date</span>
+                                <span> {{ $order['created_at'] }} </span>
+                            </div>
+                            <div>
+                                <span>payment provider</span>
+                                <span> {{ $order['payment_provider'] }} </span>
+                            </div>
+                            <div>
+                                <span>Total</span>
+                                <span> {{ $order['total'] }} {{ $order['currency'] }}</span>
+                            </div>
+                        </div>
+                        <div class="flex gap-1">
+                            <button
+                                class="px-4 py-2 rounded-[4px] bg-indigo-50 text-xs font-semibold hover:bg-indigo-200">Detail</button>
+                            <button
+                                class="px-4 py-2 rounded-[4px] bg-indigo-50 text-xs font-semibold hover:bg-indigo-200">Cancel</button>
+                            <button
+                                class="px-4 py-2 rounded-[4px] bg-indigo-50 text-xs font-semibold hover:bg-indigo-200">Detail</button>
+                            <button
+                                class="px-4 py-2 rounded-[4px] bg-indigo-50 text-xs font-semibold hover:bg-indigo-200">Go
+                                to
+                                Payment</button>
+                        </div>
+                        {{-- {{ print_r($order) }} --}}
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
